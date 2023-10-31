47af6832c955418af2fb7b58802617f7aef2ba3a
Lucia Perez Saignac <lip2110@columbia.edu>
2023-10-31 18:35:36 -0400

    Initial Commit

---
+++ ColumbiaMosaic/ColumbiaMosaicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColumbiaMosaicApp.swift
+//  ColumbiaMosaic
+//
+//  Created by Lucia Perez-Saignac on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct ColumbiaMosaicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
